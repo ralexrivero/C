003610c1df32f82156345b94fa7a214c108e3c5e
ralexrivero <ralexrivero@gmail.com>
2021-07-21 12:15:29 -0300

    function pointer arguments

---
+++ function_pointer/01-function_pointer_argument.c
@@ -0,0 +1,21 @@
+/* function pointers */
+
+/* two functions */
+
+void fun1() { printf("Fun1\n");}
+void fun2() { printf("Fun2\n");}
+
+/* function that receives a simple function */
+/* as parameter and call the function */
+
+void wrapper(void (*fun)())
+{
+    fun();
+}
+
+int main()
+{
+    wrapper(fun1);
+  /*  wrapper(fun2); */
+    return (0);
+}
