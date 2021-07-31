e19018301ca0b4b5d136d2b94219e1a9de17f1e4
ralexrivero <ralexrivero@gmail.com>
2021-07-31 12:32:28 -0300

    training pointers

--- pointers/01-00-pointers.c
+++ pointers/01-00-pointers.c
@@ -2,5 +2,16 @@
 
 int main(void)
 {
-    
+ /* declaracion de variables puntero */
+ int *pa;
+ char *pc;
+ float *pf;
+
+pa = 24;
+
+ printf("valor de pa: %d\n", pa);
+ printf("direccion de pa: %d\n", &pa);
+ printf("direccion de pa: %x\n", &pa);
+ printf("valor de puntero pa: %lli\n", *pa);
+
 }
---
+++ pointers/01-00-pointers_01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char s[] = "asd";
+    char **p = &s;
+
+    printf("The value of s is: %p\n", s);
+    printf("The direction of s is: %p\n", &s);
+
+    printf("The value of p is: %p\n", p);
+    printf("The direction of p is: %p\n", &p);
+
+    printf("The direction of s[0] is: %p\n", &s[0]);
+    printf("The direction of s[1] is: %p\n", &s[1]);
+    printf("The direction of s[2] is: %p\n", &s[2]);
+
+    return 0;
+}
