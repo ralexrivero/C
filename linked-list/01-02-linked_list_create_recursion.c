4a1e9c8fb93b08dce4a942645ed0efc57c9e729d
ralexrivero <ralexrivero@gmail.com>
2021-08-04 21:03:15 -0400

    recursive linked list

---
+++ linked-list/01-02-linked_list_create_recursion.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * string_to_list - create a linear linked list
+ * Description: using recursion
+ * @s: string to produce the list
+ */
+
+LINK string_to_list(char s[])
+{
+    LINK head;
+/**
+ * when pass an string as argument create a list with the characters
+ */
+    if(s[0] == '\0')
+    return (NULL);
+/* when the end of the string is detected return NULL */
+/* terminates the recursion */
+    else
+    {
+        head = malloc(sizeof(ELEMENT));
+/* if s[] is not the null string, malloc() retrieves enough bytes */
+/* to store an object type ELEMENT */
+        head -> d = s[0];
+/* the memeber d of ELEMENT is assigned the first character of string */
+        head -> next = string_to_list(s + 1);
+/* pointer expresion: s + 1 points the remainder of string */
+/* call recursively */
+        return (head);
+/* function exits and return the address of the head of the list */
+    }
+}
+
+/**
+ * main - for testing prototype
+ */
+
+int main(void)
+{
+    char s[] = "hola";
+
+    string_to_list(s);
+
+    
+}
