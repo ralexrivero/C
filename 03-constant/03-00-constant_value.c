fcf1c4a269c4b2c076be176767116b630799f94b
ralexrivero <ralexrivero@gmail.com>
2021-08-09 15:55:55 -0400

    constant and reorder files and dirs

---
+++ 03-constant/03-00-constant_value.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+        const char greetings[] = "Hello friends!";
+        const int cVarInt_n = 105;
+
+        puts(greetings);
+        /* can not increment or chage the value of constant */
+        printf("value: %d\n", cVarInt_n);
+        return (0);
+}
