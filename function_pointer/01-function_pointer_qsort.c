7fa4e0e84d5bb1907614aa40e823c2f5054ca30f
ralexrivero <ralexrivero@gmail.com>
2021-08-10 23:35:47 -0300

    fix description

--- function_pointer/01-function_pointer_qsort.c
+++ function_pointer/01-function_pointer_qsort.c
@@ -5,7 +5,7 @@
 
 /** comparator function
  * sorting an integer array in ascending order
- * to sort any array for anay other data type and/or
+ * to sort any array for any other data type and/or
  * criteria, all we need to do is write more compare
  * function. and we can use the same qsort()
  */
