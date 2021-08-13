6f7934d96c2c1870447b3492b26ed3f40a4aa2d8
ralexrivero <ralexrivero@gmail.com>
2021-08-13 09:40:38 -0400

    typedef

---
+++ 05-structures/05-00-typedef_structure.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+        typedef struct person {
+                char name[36];
+                int age;
+        } student;
+
+        student s1;
+
+        strcpy(s1.name, "Ronald Rivero");
+        s1.age = 30;
+
+        printf("My name is %s, and my age is: %d\n", s1.name, s1.age);
+
+        return (0);
+}
