b520b3700d4d77152d84c41e094fe5e8a0921495
ralexrivero <ralexrivero@gmail.com>
2021-08-22 17:11:31 -0400

    realloc

---
+++ 0x09-malloc_free/00-00-realloc.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+        int *arr = calloc(256, sizeof(int)), i = 0;
+        if (arr == NULL)
+        {
+                perror("calloc");
+                exit (EXIT_FAILURE);
+        }
+
+        /* shrink memory */
+
+        arr = realloc(arr, 128 * sizeof(int));
+        if (arr ==NULL)
+        {
+                perror("realloc");
+                exit (EXIT_FAILURE);
+        }
+        /* sometimes the system free(arr), malloc(128 * sizeof(int)) */
+        /* if that is the case all data will loss */
+        /* solution is arr = realloc */
+        
+        for (; i < 128; i++)
+        {
+                if (i != 127)
+                printf("[%d]: %d, ", i, arr[i]);
+                else
+                printf("[%d]: %d\n", i, arr[i]);
+        }
+        free(arr);
+        return (EXIT_SUCCESS);
+}
