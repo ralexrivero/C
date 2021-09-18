93ed01e28067cb3978716e6c87d8970df834dd91
ralexrivero <ralexrivero@gmail.com>
2021-09-17 23:44:38 -0400

    set tabulation to tabs

--- 0x05-structures/05-00-typedef_structure.c
+++ 0x05-structures/05-00-typedef_structure.c
@@ -3,17 +3,17 @@
 
 int main(void)
 {
-        typedef struct person {
-                char name[36];
-                int age;
-        } student;
+	typedef struct person {
+		char name[36];
+		int age;
+	} student;
 
-        student s1;
+	student s1;
 
-        strcpy(s1.name, "Ronald Rivero");
-        s1.age = 30;
+	strcpy(s1.name, "Ronald Rivero");
+	s1.age = 30;
 
-        printf("My name is %s, and my age is: %d\n", s1.name, s1.age);
+	printf("My name is %s, and my age is: %d\n", s1.name, s1.age);
 
-        return (0);
+	return (0);
 }
