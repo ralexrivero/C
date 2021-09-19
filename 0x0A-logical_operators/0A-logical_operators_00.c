a53253437ce3396517aaa522ca19812aca58335f
ralexrivero <ralexrivero@gmail.com>
2021-09-19 00:02:27 -0400

    logical operators

---
+++ 0x0A-logical_operators/0A-logical_operators_00.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/* logical operators works on expressions that  evaluate TRUE or FALSE */
+	/* && logical AND */
+	/* || logical OR (two pipes or vertical bars */
+	/* the value 0 is false */
+	/* Non zero values are true */
+
+	 int a = 0;
+
+	 while (a <= 20)
+	 	{
+			 if (a > 6 && a < 15)
+			 	putchar('*');
+			else
+				putchar(' ');
+			printf("%3d\12", a);
+			a++;
+		 }
+	a = 0;
+
+	putchar('\12');
+	
+	while (a <= 20)
+		{
+			if (a < 6 || a > 15)
+				putchar('*');
+			else
+				putchar(' ');
+				printf("%3d\12",a);
+			a++;
+		}
+	return (0);
+}
