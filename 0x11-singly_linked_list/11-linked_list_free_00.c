57c434fd13aa8e2ad321adc92b7a539cb4506f5b
ralexrivero <ralexrivero@gmail.com>
2021-10-09 15:59:34 -0300

    free linked list

--- 0x11-singly_linked_list/11-linked_list_free_00.c
+++ 0x11-singly_linked_list/11-linked_list_free_00.c
@@ -1,3 +1,5 @@
+/* #define _CRTDGB_MAP_ALLOC
+#include <crtdbg.h> */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -22,9 +24,8 @@ void insert_end(node **root, int value)
 	{
 		exit(1);
 	}
-	new_node->next = NULL;
 	new_node->x = value;
-	/* check if the list is empty */
+	/* verifiy if the list is empty */
 	if (*root == NULL)
 	{
 		*root = new_node;
@@ -39,6 +40,18 @@ void insert_end(node **root, int value)
 	current->next = new_node;
 }
 
+void deaolocate_list(node **root)
+{
+	node* current = *root;
+	while(current != NULL)
+	{
+		node* temp = current;
+		current = current->next;
+		free(temp);
+	}
+	*root = NULL;
+}
+
 int main()
 {
 	node* root = NULL;
@@ -50,5 +63,7 @@ int main()
 	for (node* current = root; current != NULL; current = current->next)
 	printf("%d\n", current->x);
 
+	deaolocate_list(&root);
+/*         _CrtDumpMemoryLeaks(); */
 	return (0);
 }
