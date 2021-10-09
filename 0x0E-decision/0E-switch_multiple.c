e13fb8513514f6a3f7414edcb42103650187a1ec
ralexrivero <ralexrivero@gmail.com>
2021-10-09 10:53:36 -0300

    switch multiple

---
+++ 0x0E-decision/0E-switch_multiple.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char c = 0;
+
+	printf("Select your choice:\n");
+	printf("A - Piza\nB - Chesseburger\nC - Sushi\n");
+	scanf("%c", &c);
+
+	switch (c)
+	{
+		case 'A':
+		case 'a':
+			printf("Great! a piza!\n");
+			break;
+		case 'B':
+		case 'b':
+			printf("Yeah! Cheeseburger with fries!\n");
+			break;
+		case 'C':
+		case 'c':
+			printf("I love suhi!\n");
+			break;
+		default:
+			printf("I am hungry! Select something!\n");
+	}
+	return (0);
+}
